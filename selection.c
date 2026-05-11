e01e686586a1e6712087f04daa43b4309d8957a7
In-my-veins <mucyoasifiwe80@gmail.com>
2026-05-11 09:13:02 +0200

    More on insertion sorts

--- selection.c
+++ selection.c
@@ -1,15 +1,25 @@
 #include <stdio.h>
+int main(){
 
-void selectSort(int array[],int n){
-    for(int i = 0; i < n; i++){
-        int min_pos = i;
-        for(int j = i+1; j < n - 1; j++)
-        if(array[j] < array[min_pos]) min_pos = j;
+int array[] = {5,9,7,6,4,0,2,3,8,1};
+
+for(int i = 0; i < 10; i++){
+    int min_pos = i;
+    for(int j = i+1; j < 10; j++){
+        if(array[j] < array[min_pos]){
+            min_pos = j;
+        }
     }
+    if(min_pos != i){
+            int temp = array[i];
+            array[i] = array[min_pos];
+            array[min_pos] = temp;
+        }
 }
 
-
-int main(){
+for(int i = 0; i < 10; i++){
+    printf("%d\n",array[i]);
+}
 
     return 0;
 }
